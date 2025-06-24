3a383b0a71d73f07ef24853cd6173e99c2e22294
Detrixbwu <157987800+Detrixbwu@users.noreply.github.com>
2025-06-24 13:46:01 -0600

    start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# Guardar rclone.conf en archivo
-echo "$RCLONE_CONF" > rclone.conf
+echo "$RCLONE_CONF" > /rclone.conf
 
-# Instalar rclone
-curl https://rclone.org/install.sh | bash
+rclone --config /rclone.conf mount gdrive:/JellyfinLibrary /mnt/drive --vfs-cache-mode writes &
 
-# Crear carpeta para montar Google Drive
-mkdir -p /mnt/drive
+service jellyfin start
 
-# Montar Google Drive (con la carpeta JellyfinLibrary)
-rclone --config rclone.conf mount gdrive:/JellyfinLibrary /mnt/drive --vfs-cache-mode writes &
-
-# Instalar Jellyfin
-sudo apt-get update
-sudo apt-get install -y jellyfin
-
-# Iniciar Jellyfin
-sudo service jellyfin start
-
-# Mantener el script corriendo para que no se cierre
 tail -f /dev/null
